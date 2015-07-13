a90dedfae779024c1afb62aef25b6fa53747bdf0
Levi Buzolic <levibuzolic@gmail.com>
2015-07-13 19:17:16 +1000

    New API

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var app, express, getTimesForStop, getUrlForStop, logfmt, moment, newrelic, parseDateString, port, request, server;
+var app, express, getTimesForStop, getUrlForStop, logfmt, moment, parseDateString, port, request, server;
 
-newrelic = require('newrelic');
 express = require('express');
 request = require('request');
 moment = require('moment');
@@ -14,8 +13,35 @@ server = app.listen(port, function() {
   return console.log('Listening on port ' + port);
 });
 
+/*
+
+iOS API
+-------
+http://ws3.tramtracker.com.au/TramTracker/restservice/GetNextPredictedRoutesCollection/1234/0/false/?aid=TTIOSJSON&cid=2&tkn=F570461F-8AA4-4626-969A-B48A0659B2DF
+
+Web API (Broken)
+----------------
+http://www.tramtracker.com.au/Controllers/GetNextPredictionsForStop.ashx?stopNo=1234&routeNo=0&isLowFloor=false
+
+Web API 2
+---------
+POST: StopID=1234&Route=&LowFloorOnly=false
+http://yarratrams.com.au/base/tramTrackerController/TramInfoAjaxRequest
+
+*/
+
+var getRandomNumber = function() {
+  return Math.floor(Math.random() * 10);
+};
+
+var getRandomToken = function() {
+  // return 'F570461F-8AA4-4626-969A-B48A0659B2DF';
+  return ['F57', getRandomNumber(), getRandomNumber(), '61F-8AA4-4626-969A-B48A0659B2DF'].join();
+};
+
 getUrlForStop = function(stop) {
-  return "http://www.tramtracker.com.au/Controllers/GetNextPredictionsForStop.ashx?stopNo=" + stop + "&routeNo=0&isLowFloor=false";
+  return "http://ws3.tramtracker.com.au/TramTracker/restservice/GetNextPredictedRoutesCollection/" + stop + "/0/false/?aid=TTIOSJSON&cid=2&tkn=" + getRandomToken();
+  // return "http://www.tramtracker.com.au/Controllers/GetNextPredictionsForStop.ashx?stopNo=" + stop + "&routeNo=0&isLowFloor=false";
 };
 
 parseDateString = function(string) {
