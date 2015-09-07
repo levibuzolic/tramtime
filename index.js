9669311ec1861d185f3a29de4cadea27948c037a
Levi Buzolic <levibuzolic@gmail.com>
2015-09-07 17:42:25 +1000

    Add route 6

--- index.js
+++ index.js
@@ -1,107 +1,83 @@
-var app, express, getTimesForStop, getUrlForStop, logfmt, moment, parseDateString, port, request, server;
+var express = require('express');
+var request = require('request');
+var moment = require('moment');
+var logfmt = require('logfmt');
+var Promise = require('promise');
 
-express = require('express');
-request = require('request');
-moment = require('moment');
-logfmt = require('logfmt');
+var app = express();
+var port = Number(process.env.PORT || 4000);
 
-app = express();
-
-port = Number(process.env.PORT || 4000);
-
-server = app.listen(port, function() {
+var server = app.listen(port, function() {
   return console.log('Listening on port ' + port);
 });
-
-/*
-
-iOS API
--------
-http://ws3.tramtracker.com.au/TramTracker/restservice/GetNextPredictedRoutesCollection/1234/0/false/?aid=TTIOSJSON&cid=2&tkn=F570461F-8AA4-4626-969A-B48A0659B2DF
-
-Web API (Broken)
-----------------
-http://www.tramtracker.com.au/Controllers/GetNextPredictionsForStop.ashx?stopNo=1234&routeNo=0&isLowFloor=false
-
-Web API 2
----------
-POST: StopID=1234&Route=&LowFloorOnly=false
-http://yarratrams.com.au/base/tramTrackerController/TramInfoAjaxRequest
-
-*/
 
 var getRandomNumber = function() {
   return Math.floor(Math.random() * 10);
 };
 
 var getRandomToken = function() {
-  // return 'F570461F-8AA4-4626-969A-B48A0659B2DF';
   return ['F57', getRandomNumber(), getRandomNumber(), '61F-8AA4-4626-969A-B48A0659B2DF'].join();
 };
 
-getUrlForStop = function(stop) {
-  return "http://ws3.tramtracker.com.au/TramTracker/restservice/GetNextPredictedRoutesCollection/" + stop + "/0/false/?aid=TTIOSJSON&cid=2&tkn=" + getRandomToken();
-  // return "http://www.tramtracker.com.au/Controllers/GetNextPredictionsForStop.ashx?stopNo=" + stop + "&routeNo=0&isLowFloor=false";
+var getUrl = function(stop, route) {
+  route = route || 0;
+  return "http://ws3.tramtracker.com.au/TramTracker/restservice/GetNextPredictedRoutesCollection/" + stop + "/" + route + "/false/?aid=TTIOSJSON&cid=2&tkn=" + getRandomToken();
 };
 
-parseDateString = function(string) {
+var parseDateString = function(string) {
   var matches;
   matches = string.match(/\/Date\((\d+)\+\d+\)\//);
   return moment(Number(matches[1]));
 };
 
-getTimesForStop = function(stop, callback) {
-  if (stop == null) {
-    stop = 1234;
-  }
-  return request(getUrlForStop(stop), function(apiError, apiResponse, apiBody) {
-    var arrivalTime, json, minutes, number, results, string, time, timeResponded, times;
-    json = JSON.parse(apiBody);
-    timeResponded = parseDateString(json.timeResponded);
-    times = json.responseObject || [];
-    results = (function() {
-      var i, len, results1;
-      results1 = [];
-      for (i = 0, len = times.length; i < len; i++) {
-        time = times[i];
-        arrivalTime = parseDateString(time.PredictedArrivalDateTime);
-        number = time.RouteNo;
-        minutes = (arrivalTime - timeResponded) / 1000 / 60;
-        string = arrivalTime.from(timeResponded, true);
-        results1.push({
-          number: number,
-          minutes: minutes,
-          string: string
-        });
-      }
-      return results1;
-    })();
-    return typeof callback === "function" ? callback(results) : void 0;
+var getTimes = function(stop, route, callback) {
+  return new Promise(function(resolve, reject){
+    request(getUrl(stop, route), function(apiError, apiResponse, apiBody) {
+      var arrivalTime, json, minutes, number, results, string, time, timeResponded, times;
+      json = JSON.parse(apiBody);
+      timeResponded = parseDateString(json.timeResponded);
+      times = json.responseObject || [];
+      results = (function() {
+        var i, len, results1;
+        results1 = [];
+        for (i = 0, len = times.length; i < len; i++) {
+          time = times[i];
+          arrivalTime = parseDateString(time.PredictedArrivalDateTime);
+          number = time.RouteNo;
+          minutes = (arrivalTime - timeResponded) / 1000 / 60;
+          string = arrivalTime.from(timeResponded, true);
+          results1.push({
+            number: number,
+            minutes: minutes,
+            string: string
+          });
+        }
+        return results1;
+      })().map(function(routeTime) {
+        return routeTime.string;
+      }).join(', ');
+
+      resolve("*Route "+route+":* " + results);
+    });
   });
 };
 
 app.use(logfmt.requestLogger());
 
 app.post('/ferocia', function(req, res) {
-  return getTimesForStop(1234, function(route1) {
-    return getTimesForStop(1396, function(route12) {
-      var response, route12times, route1times;
-      route1times = route1.map(function(time) {
-        return time.string;
-      }).join(', ');
-      route12times = route12.map(function(time) {
-        return time.string;
-      }).join(', ');
-      response = {
-        text: "Did somebody mention *trams*?\n*Route 1:* " + route1times + "\n*Route 12:* " + route12times
-      };
-      return res.send(response);
+  Promise.all([
+    getTimes(1234, 1),
+    getTimes(1396, 12),
+    getTimes(3020, 6)
+  ]).then(function(results) {
+    res.send({
+      text: ["Did somebody mention *trams*?"].concat(results).join("\n")
     });
   });
 });
 
 app.get('/stop/:id', function(req, res) {
-  return getTimesForStop(parseInt(req.params.id), function(results) {
+  return getTimes(parseInt(req.params.id) || 1234, function(results) {
     return res.send(results);
   });
 });
